d0e5464844874abc45bd24057cade164f8863728
Gael Rogelio Bejarano Dominguez <a.22308051281154@cbtis128.edu.mx>
2024-09-20 09:21:50 -0600

    Add files via upload

---
+++ OperadoresA.py
@@ -0,0 +1,68 @@
+print("Act 9 clases V3 \nRogelio Bejarano Mat: 22308051281154")
+# Zona de clases
+class Operadores1154:
+    # Zona de funciones 
+    def suma1154(self,B,D):
+        print("A continuacion una suma de 2 factores")
+        s1154 = B + D
+        print(f"La suma de {B} y {D} =",s1154)
+    def resta1154(self,B,D):
+        print("A continuacion una resta de 2 factores")
+        r1154 = B - D
+        print(f"La resta de {B} - {D} =",r1154)
+    def multi1154(self,B,D):
+        print("A continuacion una multiplicacion de 2 factores")
+        r1154 = B * D
+        print(f"La multiplicacion de {B} x {D} =",r1154)
+    def div1154(self,B,D):
+        print("A continuacion una division de 2 factores")
+        r1154 = B / D
+        print(f"La division de {B} / {D} =",r1154)
+    def ex1154(self,B,D):
+        print("A continuacion un exponente")
+        r1154 = B ** D
+        print(f"{B} ^ {D} =",r1154)
+    def MOD1154(self,B,D):
+        print("A continuacion un MOD")
+        r1154 = B % D
+        print(f"{B} % {D} =",r1154)
+    def coc1154(self,B,D):
+        print("A continuacion un cociente")
+        r1154 = B // D
+        print(f"{B} // {D} =",r1154)
+# Zona de creacion del objeto
+OpRgr = Operadores1154()
+# Zona de uso de objetos
+print("\nFuncion suma")
+OpRgr.suma1154(5,4)
+print("\nFuncion division")
+OpRgr.div1154(8,2)
+print("\nFuncion exponente")
+OpRgr.ex1154(23,5)
+print("\nFuncion multiplicacion")
+OpRgr.multi1154(14,72)
+print("\nFuncion resta")
+OpRgr.resta1154(10,1.72)
+print("\nFuncion cociente")
+OpRgr.coc1154(61,3)
+print("\nFuncion de MOD")
+OpRgr.MOD1154(9,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
